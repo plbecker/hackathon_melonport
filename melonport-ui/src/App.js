2eb248c8281a7884c01c98296cff7a4df9b85e96
pr0duktiv <patrice@5becker.de>
2018-05-18 01:04:26 +0200

    simple list

--- melonport-ui/src/App.js
+++ melonport-ui/src/App.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid/dist/styles/ag-grid.css';
+import 'ag-grid/dist/styles/ag-theme-balham.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                {headerName: "Make", field: "make"},
+                {headerName: "Model", field: "model"},
+                {headerName: "Price", field: "price"}
+
+            ],
+            rowData: [
+                {make: "Toyota", model: "Celica", price: 35000},
+                {make: "Ford", model: "Mondeo", price: 32000},
+                {make: "Porsche", model: "Boxter", price: 72000}
+            ]
+        }
+    }
+    render() {
+        return (
+            <div  className="ag-theme-balham"
+            style={{ 
+                height: '500px', 
+                    width: '600px' }}>
+            <AgGridReact columnDefs={this.state.columnDefs} rowData={this.state.rowData}>
+            </AgGridReact>
+            </div>
+        );
+    }
 }
 
 export default App;
